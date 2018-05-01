6e371949673bcb4be7d3e822bd4d9e9d098ca333
otromimi <35479397+otromimi@users.noreply.github.com>
2018-05-01 16:25:51 +0200

    Menu recursibo

--- main.c
+++ main.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+int menu();
+
+
+
+int main(){
+
+    switch(menu()){
+        case 0:
+            break;
+        case 1:
+            system("cls");
+            printf("opcion 1");
+            break;
+        case 2:
+            system("cls");
+            printf("opcion 2");
+            break;
+        case 3:
+            system("cls");
+            printf("opcion 3");
+            break;
+        case 4:
+            system("cls");
+            printf("opcion 4");
+            break;
+    }
+
+
+
+
+
+
+}
+
+
+int menu(){
+
+    int opcion;
+
+    system("cls");
+    printf("\n\tMENU PRINCIPAL\n\n\t1.-Combrobar ultimo numero\n\t2.-Escribir secuencia invertida y replicada parcialmente\n\t3.-Escribir secuencia invertida y replicada totalmente\n\t4.-Comprobar primer numero y suma\n\n\t0.-Salir\n\n\t->");
+    scanf("%i",&opcion);
+    if(opcion<0||opcion>4){
+        system("cls");
+        printf("\n\tLa opcion seleccionada no es valida.\n\n");
+        system("pause");
+        opcion=menu();
+    }
+    return opcion;
 }
