60f337c700bf30a513dd3c0fa0035109568d6a71
otromimi <35479397+otromimi@users.noreply.github.com>
2018-05-04 12:51:57 +0200

    update

--- main.c
+++ main.c
@@ -8,26 +8,35 @@ int menu();
 
 int main(){
 
-    switch(menu()){
-        case 0:
-            break;
-        case 1:
-            system("cls");
-            printf("opcion 1");
-            break;
-        case 2:
-            system("cls");
-            printf("opcion 2");
-            break;
-        case 3:
-            system("cls");
-            printf("opcion 3");
-            break;
-        case 4:
-            system("cls");
-            printf("opcion 4");
-            break;
-    }
+    int opcion;
+    do{
+        opcion=menu();
+        switch(opcion){
+            case 0:
+                break;
+            case 1:
+                system("cls");
+                printf("\n\tOPCION 1.- COMBREOBAR EL ULTIMO NUMERO\n\n");
+                printf("---------------------------------------------------------");
+                printf("\n\n Introduzca una secuencia de numeros positivos..\n <Un numero en cada linea, 0 para terminar>\n\n");
+                opcion1();
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                printf("opcion 2");
+                break;
+            case 3:
+                system("cls");
+                printf("opcion 3");
+                break;
+            case 4:
+                system("cls");
+                printf("opcion 4");
+                break;
+        }
+
+    }while(opcion!=0);
 
 }
 
@@ -52,7 +61,25 @@ int menu(){
 
 
 
-void opcion1(){}
+int opcion1(){
+
+    int num;
+
+    printf("\r Introduce un numero: ");
+    scanf("%i",&num);
+    if(num<0){
+        //printf("\r");
+        opcion1();
+    }else{
+        printf("\n");
+        if(num=0){
+            printf("\n\n\tEl ultimo valor es %i",num);
+        }else{
+            opcion1();
+        }
+    }
+ return num;
+}
 
 void opcion2(){}
 
