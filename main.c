d42c534eea11eeaa928b4c6f96a6a58688f36b95
otromimi <35479397+otromimi@users.noreply.github.com>
2018-05-11 01:41:12 +0200

    opcion2

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int menu();
 
 int main(){
 
-    int opcion,cont;
+    int opcion,cont,last;
     do{
         opcion=menu();
         switch(opcion){
@@ -21,12 +21,17 @@ int main(){
                 printf("---------------------------------------------------------");
                 printf("\n\n Introduzca una secuencia de numeros positivos..\n <Un numero en cada linea, 0 para terminar>\n\n");
                 cont=0;
-                opcion1(&cont);
+                last=opcion1(&cont);
+                printf("\n\n\tSe introducieron %i numeros con el ultimo valor %i.\n\n  ",cont,last);
                 system("pause");
                 break;
             case 2:
                 system("cls");
-                printf("opcion 2");
+                printf("\n\tOPCION 2.- SECUENCIA INVERTIDA Y REPLICADA PARCIALMENTE\n\n");
+                printf("---------------------------------------------------------");
+                printf("\n\n Introduzca una secuencia de numeros positivos, 0 para terminar.\n\t");
+                opcion2();
+                system("pause");
                 break;
             case 3:
                 system("cls");
@@ -67,29 +72,50 @@ int opcion1(int *p_cont){
 
     int num=0,last=0;
 
-
     printf("\n Introduce un numero: ");
     scanf("%i",&num);
     if(num<0){
-
         printf("\t\t\tNumero invalido.\n");
         opcion1(p_cont);
-
     }else if(num>0){
-
         ++*p_cont;
-        opcion1(p_cont);
-
-
+        last=opcion1(p_cont);
+        if(last==0){
+            return num;
+        }else{
+            return last;
+        }
     }else{
-
-        printf("\n\n\tSe introducieron %i numeros con el ultimo valor %i.\n\n  ",*p_cont,last);
-
+        return num;
     }
-    return num;
 }
 
-void opcion2(){}
+int opcion2(){
+
+    int i,num=0,devuelto=0;
+
+
+    scanf("%i",&num);
+    if(num<0){
+        opcion2();
+    }else if(num>0){
+
+        devuelto=opcion2();
+        if(devuelto==0){
+            for(i=0;i<num;i++){
+                printf("%i ",num);
+            }
+        }else{
+            printf("%i ",num);
+        }
+    }else{
+        printf("%i\n\n\t",0);
+        printf("La secuencia es: ");
+        return num;
+    }
+
+
+}
 
 void opcion3(){}
 
